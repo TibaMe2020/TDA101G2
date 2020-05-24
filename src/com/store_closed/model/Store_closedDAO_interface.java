1f790483e0a83007e85ed5e3eaf782978cbdc016
joey155343 <joey155343@hotmaail.com>
2020-05-24 16:42:32 +0800

    Store_closedService

--- src/com/store_closed/model/Store_closedDAO_interface.java
+++ src/com/store_closed/model/Store_closedDAO_interface.java
@@ -4,6 +4,6 @@ import java.util.Set;
 
 public interface Store_closedDAO_interface {
 	public void insert(Store_closedVO store_closedVO);
-	public void delete(String store_id);
+	public void delete(String store_closed_id);
 	public Set<Store_closedVO> selectByStore(String store_id);
 }
